64350a4bb6e6691df6f1916d9ef10d1ab6755ec5
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2025-05-03 21:49:02 +0100

    Rope Minigame Functionality
    Scripting wise, this is basically finished, just needs a few touch ups and minor bug fixes

--- Assets/Scripts/MinigameScripts/Rope.cs
+++ Assets/Scripts/MinigameScripts/Rope.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rope : MonoBehaviour
 {
     public Knot knotA;
     public Knot knotB;
+    public bool isCut;
+    //[SerializeField] private Sprite[] sprites = new Sprite[2];
+
+    private BoxCollider2D col;
+    private Image image;
+
+    private void Awake()
+    {
+        col = GetComponent<BoxCollider2D>();
+        image = GetComponent<Image>();
+        image.color = Color.white;
+    }
+
+    public void Cut()
+    {
+        if (isCut) { return; }
+        isCut = true;
+        col.enabled = false;
+        image.color = Color.red;
+    }
 }
--- Assets/Scripts/MinigameScripts/RopeMinigame.cs
+++ Assets/Scripts/MinigameScripts/RopeMinigame.cs
@@ -1,11 +1,16 @@
-using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
 public class RopeMinigame : MonoBehaviour
 {
     public Rope[] ropes;
     public Knot[] knots;
     private List<int> knotsList = new List<int>();
+    [SerializeField] private GameObject failureWindow;
+    [SerializeField] private GameObject successWindow;
+    private HashSet<Rope> cutRopes = new HashSet<Rope>();
+    private bool gameOver;
 
     private void Awake()
     {
@@ -17,9 +22,107 @@ public class RopeMinigame : MonoBehaviour
         AssignKnotTypes();
     }
 
+    private void Update()
+    {
+        if (!gameOver)
+        {
+            if (Input.GetMouseButtonDown(0)) { ProcessCut(); }
+        }
+    }
+
+    void ProcessCut()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, -Vector2.up);
+        if (hit.collider == null) { return; }
+        Rope rope = hit.collider.GetComponent<Rope>();
+        if (rope == null || cutRopes.Contains(rope)) { return; }
+        else
+        {
+            hit.collider.GetComponent<Rope>().Cut();
+            cutRopes.Add(rope);
+        }
+    }
+
+    void Fail(string failMessage)
+    {
+        gameOver = true;
+        failureWindow.SetActive(true);
+        Debug.Log("Fail with message: " + failMessage);
+        failureWindow.GetComponentInChildren<TextMeshProUGUI>().text = failMessage;
+    }
+    void Succeed()
+    {
+        Debug.Log("won");
+        successWindow.SetActive(true);
+    }
+
+    public void Restart()
+    {
+
+    }
+
+    public void Finish()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void ValidateRopeLayout()
+    {
+        var touchedCounts = new Dictionary<Knot, int>();
+        foreach (var rope in cutRopes)
+        {
+            if (!touchedCounts.ContainsKey(rope.knotA)) { touchedCounts[rope.knotA] = 0; }
+            if (!touchedCounts.ContainsKey(rope.knotB)) { touchedCounts[rope.knotB] = 0; }
+
+            touchedCounts[rope.knotA]++;
+            touchedCounts[rope.knotB]++;
+            if (rope.knotA.myKnotType == Knot.KnotType.KnotTight || rope.knotB.myKnotType == Knot.KnotType.KnotTight)
+            {
+                Debug.Log("Hit rope = " + rope + ", and hit knots = " + rope.knotA.myKnotType + " & " + rope.knotB.myKnotType);
+                Fail("You cut a tight knot!");
+                return;
+            }
+        }
+        Debug.Log("passed first loop");
+
+        foreach (var knot in knots)
+        {
+            if (knot.myKnotType == Knot.KnotType.KnotFrayed)
+            {
+                if (touchedCounts.TryGetValue(knot, out int count) && count >= 2) { continue; }
+
+                Fail("A frayed knot isn't cut enough!");
+                return;
+            }
+        }
+        Debug.Log("passed second loop");
+
+        int expectedCuts = 0;
+        foreach (var knot in knots)
+        {
+            if (knot.myKnotType == Knot.KnotType.KnotFrayed)
+            {
+                expectedCuts += touchedCounts.ContainsKey(knot) ? touchedCounts[knot] : 0;
+            }
+        }
+        Debug.Log("passed third loop");
+        expectedCuts /= 2;
+
+        if (cutRopes.Count >= expectedCuts)
+        {
+            Succeed();
+        }
+        else
+        {
+            Debug.Log("so close mate");
+        }
+    }
+
     void AssignKnotTypes()
     {
         ShuffleKnotsList();
+        // Assigning 5 regular, 4 frayed and 3 tight knots
+        // 4/4/4 was causing too many unsolveable layouts
         for (int i = 0; i < 5; i++)
         {
             knots[knotsList[i]].myKnotType = Knot.KnotType.Knot;
@@ -38,6 +141,7 @@ public class RopeMinigame : MonoBehaviour
             knot.AssignSprite();
         }
 
+        // Validate current layout, if invalid then reshuffle
         if (!ValidateKnotLayout())
         {
             Debug.LogWarning("Invalid layout generated, reshuffling");
@@ -62,13 +166,17 @@ public class RopeMinigame : MonoBehaviour
         List<int> frayedKnots = new List<int>();
         HashSet<int> tightKnots = new HashSet<int>();
 
+        // Collecting frayed and tight knot indexes
         for (int i = 0; i < knots.Length; i++)
         {
             if (knots[i].myKnotType == Knot.KnotType.KnotFrayed) { frayedKnots.Add(i); }
             else if (knots[i].myKnotType == Knot.KnotType.KnotTight) { tightKnots.Add(i); }
         }
 
+        // Building knot graph temporarily excluding tight knots
         Dictionary<int, List<int>> fullGraph = BuildKnotGraph();
+
+        // Check if frayed knots are correctly connected and not isolated
         foreach (int frayedIndex in frayedKnots)
         {
             if (!fullGraph.ContainsKey(frayedIndex)) { return false; }
@@ -79,9 +187,11 @@ public class RopeMinigame : MonoBehaviour
                 if (!tightKnots.Contains(neighbor)) { validNeighborCount++; }
             }
 
+            // if a frayed knot has less than 2 valid neighbours, it is completely unsolveable
             if (validNeighborCount < 2) { return false; }
         }
 
+        // Check if frayed knots are all reachable
         Dictionary<int, List<int>> filteredGraph = BuildKnotGraph(tightKnots);
         HashSet<int> visited = new HashSet<int>();
         DepthFirstSearch(frayedKnots[0], filteredGraph, visited);
@@ -90,8 +200,8 @@ public class RopeMinigame : MonoBehaviour
         {
             if (!visited.Contains(frayedIndex)) { return false; }
         }
-        
-        return true;
+
+        return true; // If this return statement is run then the layout is detected as valid
     }
 
     Dictionary<int, List<int>> BuildKnotGraph()
@@ -102,14 +212,13 @@ public class RopeMinigame : MonoBehaviour
     Dictionary<int, List<int>> BuildKnotGraph(HashSet<int> blockedKnots)
     {
         Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
-        
+
         foreach (Rope rope in ropes)
         {
-            //int a = rope.knotA;
-            //int b = rope.knotB;
             int a = System.Array.IndexOf(knots, rope.knotA);
             int b = System.Array.IndexOf(knots, rope.knotB);
 
+            // Skip if the rope contains blocked knots
             if (blockedKnots.Contains(a) || blockedKnots.Contains(b)) { continue; }
 
             if (!graph.ContainsKey(a)) graph[a] = new List<int>();
